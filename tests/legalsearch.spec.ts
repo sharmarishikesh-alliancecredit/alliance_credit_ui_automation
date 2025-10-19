54bb2deefa405fc6da2980892078d126668c4ccf
unknown <sharmarishikesh12.15@gmail.com>
2025-10-19 22:38:05 +0530

    allure report

--- tests/legalsearch.spec.ts
+++ tests/legalsearch.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from '@playwright/test';
 
+const url: string = 'http://localhost:5173/';
+
 test('Verify if main table in legal-search section is visible', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto(url);
   await page.getByText('Login As Admin').click();
   await expect(page.locator('div').filter({ hasText: 'File Ref. IdFileFile' }).nth(4)).toBeVisible();
 });
 
 test('Verify if the legal search button is clickable and redirects to search page', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto(url);
   await page.getByText('Login As Admin').click();
   await expect(page.getByText('Legal Search')).toBeEnabled();
   await page.getByText('Legal Search').click();
@@ -15,7 +17,7 @@ test('Verify if the legal search button is clickable and redirects to search pag
 });
 
 test('Verify if the search functionality is working as expected', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto(url);
   await page.getByText('Login As Admin').click();
   await page.getByPlaceholder('Search Legal File').click();
   await page.getByPlaceholder('Search Legal File').fill('jlr');
@@ -24,5 +26,17 @@ test('Verify if the search functionality is working as expected', async ({ page 
   await page.getByRole('img').nth(2).click();
   await page.getByPlaceholder('Search Legal File').fill('LG-002568');
   await expect(page.getByRole('main')).toContainText('LG-002568');
+});
+
+test('Verify if the upload functionality is working as expected', async ({ page }) => {
+  await page.goto(url);
+  await page.getByText('Login As Admin').click();
+  await page.getByRole('button', { name: 'Upload Legal' }).click();
+  await page.getByRole('textbox', { name: 'Legal Type' }).click();
+  await page.getByText('bkr_select').click();
+  await page.getByRole('textbox', { name: 'Region' }).click();
+  await page.getByText('eastern').click();
+  await page.locator("//div[@class='custom_upload']//child::input").setInputFiles('LegalFiles/BKR.xls');
+  await page.getByRole('button', { name: 'Upload', exact: true }).click();
 });
 
