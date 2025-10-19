7565e35b4bf116280a0898c7e702424ff0ff57d5
unknown <sharmarishikesh12.15@gmail.com>
2025-10-19 22:08:56 +0530

    Search functionality test added

--- tests/legalsearch.spec.ts
+++ tests/legalsearch.spec.ts
@@ -1,18 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-
-
 test('Verify if main table in legal-search section is visible', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-  await page.getByRole('button', { name: 'Login As Admin' }).click();
+  await page.getByText('Login As Admin').click();
   await expect(page.locator('div').filter({ hasText: 'File Ref. IdFileFile' }).nth(4)).toBeVisible();
 });
 
 test('Verify if the legal search button is clickable and redirects to search page', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-  await page.getByRole('button', { name: 'Login As Admin' }).click();
+  await page.getByText('Login As Admin').click();
   await expect(page.getByText('Legal Search')).toBeEnabled();
   await page.getByText('Legal Search').click();
   await expect(page.getByRole('main')).toContainText('Legal Information Search');
+});
+
+test('Verify if the search functionality is working as expected', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByText('Login As Admin').click();
+  await page.getByPlaceholder('Search Legal File').click();
+  await page.getByPlaceholder('Search Legal File').fill('jlr');
+  await page.getByText('jlr', { exact: true }).first().click();
+  await page.getByPlaceholder('Search Legal File').click();
+  await page.getByRole('img').nth(2).click();
+  await page.getByPlaceholder('Search Legal File').fill('LG-002568');
+  await expect(page.getByRole('main')).toContainText('LG-002568');
 });
 
